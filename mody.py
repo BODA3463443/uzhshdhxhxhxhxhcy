59597dd4ee07f457f66b5a4886e67c2ccd7d6960
BODA3463443 <157923113+BODA3463443@users.noreply.github.com>
2024-02-08 23:17:46 +0200

    Add files via upload

---
+++ mody.py
@@ -0,0 +1,11 @@
+import os
+
+
+class Mody(object):
+    ZERO_R7K = os.environ.get("ZERO_R7K", "")
+    
+    API_ID = os.environ.get("API_ID", "29755247")
+    
+    API_HASH = os.environ.get("API_HASH", "8dd9fb5fa2782d91b9847ace66eb885a")
+    
+
